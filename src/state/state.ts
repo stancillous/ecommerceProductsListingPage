b9c8a54cda9822cc65d955ed749f44707198eee3
stancillous <stancillousray@gmail.com>
2025-02-24 14:21:50 +0300

    store user cart in localstorage and show toast success

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia'
-
+import { useToast } from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
 
 export const useProductsStore = defineStore('products', {
     state: () => ({
         userCart: [],
-        name: "ray"
+        name: "ray",
+        toast: useToast()
     }),
     
     actions: {
@@ -22,7 +24,16 @@ export const useProductsStore = defineStore('products', {
                     productId: productId,
                     quantity: 1
                 });
+                
             }
+            // Store updated cart in localStorage
+            localStorage.setItem('userCart', JSON.stringify(this.userCart));
+            this.toast.success('Product added to cart', {
+                position: 'top-right',
+                duration: 3000,
+                dismissible: true,
+                queue: true
+            });
         },
         
         clearCart() {
